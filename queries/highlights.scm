6a4f7386c19810e486a83adf9904a70de947987d
Colin Kennedy <colinvfx@gmail.com>
2023-10-10 22:47:58 -0700

    Added WIP highlight unittests

---
+++ queries/highlights.scm
@@ -0,0 +1,21 @@
+(address) @number
+(byte) @constant
+(hexadecimal) @number
+(integer) @number
+(section_address) @number
+
+(disassembly_section_label) @text.title
+(machine_code_bytes) @field
+(file_path) @string
+(identifier) @namespace
+(instruction) @text
+(label) @label
+
+; TODO: Add the <>s back
+; "<" @punctuation.special
+; ">" @punctuation.special
+"(" @punctuation.bracket
+")" @punctuation.bracket
+":" @delimiter
+
+(comment) @comment.documentation
