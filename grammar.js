897435651b65ad52c54a8eeaa76e62aa0f927182
Colin Kennedy <colinvfx@gmail.com>
2023-10-15 22:10:11 -0700

    Got more tests to pass

--- grammar.js
+++ grammar.js
@@ -65,14 +65,79 @@ module.exports = grammar(
                 optional($.file_offset),
             ),
 
-            code_location: $ => choice(
-                seq(
-                    "<",
-                    alias(/[^>+]+/, $.identifier),
-                    optional(seq("+", $.hexadecimal)),
-                    ">",
-                ),
+            // Working?
+            // code_location: $ => prec.left(4,
+            //     seq(
+            //         "<",
+            //         alias($._code_location, $.identifier),
+            //         optional(seq("+", $.hexadecimal)),
+            //         ">",
+            //     ),
+            // ),
+
+            // _code_location: $ => prec.left(
+            //     2,
+            //     repeat1(seq(/[^>+]+/, optional($._code_location), optional(">"))),
+            // ),
+
+            // Getting closer but still doesn't work
+            // _code_location: $ => prec.left(
+            //     2,
+            //     repeat1(seq(/[^>]+/, optional($._code_location), optional(">"))),
+            // ),
+
+            // code_location: $ => prec.left(4,
+            //     seq(
+            //         "<",
+            //         alias($._code_location, $.identifier),
+            //         optional(seq("+", $.hexadecimal)),
+            //         ">",
+            //     ),
+            // ),
+            //
+            // _code_location: $ => prec.left(
+            //     2,
+            //     repeat1(choice(/[^<>]+/, $.code_location)),
+            // ),
+
+            // // WORKS!?
+            // code_location: $ => alias($.__code_location, $.identifier),
+            //
+            // _code_location: $ => prec.left(
+            //     2,
+            //     repeat1(choice(/[^<>]+/, $.__code_location)),
+            // ),
+            //
+            // __code_location: $ => seq(
+            //     "<",
+            //     $._code_location,
+            //     optional(seq("+", $.hexadecimal)),
+            //     ">",
+            // ),
+
+            code_location: $ => seq(
+                "<",
+                alias($._code_location, $.identifier),
+                optional(seq("+", $.hexadecimal)),
+                ">",
             ),
+            _code_location: $ => repeat1(choice(/[^<>+]+/, $.__code_location)),
+            __code_location: $ => seq("<", $._code_location, ">"),
+
+            // Works moderately well
+            // _code_location: $ => seq(
+            //     /[^<+]+/,
+            //     optional(seq("<", optional($._code_location), ">")),
+            // ),
+
+            // Not sure if works
+            // _code_location: $ => seq(
+            //     optional("<"),
+            //     /[^<]*/,
+            //     $._code_location,
+            //     /[^>]*/,
+            //     optional(">"),
+            // ),
 
             label_line: $ => seq(alias($._label_identifier, $.label), ":"),
 
